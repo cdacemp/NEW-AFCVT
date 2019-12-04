7cf6731d34f46d12d3fa72de1bd04e48c779bc0f
noopur <noopur2309@yahoo.in>
2019-12-04 10:52:10 -0800

    Created Shared Folder and added Header and Footer Component-Annu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/shared_component/header/header.component';
+import { FooterComponent } from './shared/shared_component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
