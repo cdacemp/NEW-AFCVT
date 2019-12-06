8095d27d74c56acfbd14963d63e5ad744968c1fa
Noopur Kumari <noopurkumari@cdacnoida.in>
2019-12-06 16:05:38 +0530

    jsbfhjsd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { DashboardComponent } from './super-dashboard/super-dashboard.component';
+import { DashboardComponent } from './super-dashboard/dashboard.component'
 
 
 const routes: Routes = [
